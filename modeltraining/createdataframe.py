39a6a94cedd419dd992c319c322031ac41265060
Jan Stawowy <stawa29@interia.pl>
2019-04-26 12:37:52 +0200

    added comments

--- modeltraining/createdataframe.py
+++ modeltraining/createdataframe.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import cv2
 import glob
 
-
+#set width and height of images
 W=32
 H=32
+#read and resize all images of infected cellsfrom a folder and label them as infected
 images = [cv2.resize(cv2.imread(file),(W,H)) for file in glob.glob("cell_images/Parasitized/*.png")]
 labels = ['infected' for i in range(len(images))]
 """
@@ -18,15 +19,19 @@ print(images[0])
 plt.imshow(images[0])
 plt.show()"""
 
+#create dataframe with infected cells and labels
 dict= {'image':images, 'state':labels}
 df_infected=pd.DataFrame(data=dict)
 
+#read and resize all images of uninfected cellsfrom a folder and label them as uninfected
 images_uninfected = [cv2.resize(cv2.imread(file),(W,H)) for file in glob.glob("cell_images/Uninfected/*.png")]
 labels_uninfected = ['uninfected' for i in range(len(images_uninfected))]
 
+#create dataframe with uninfected cells
 dict_uninf = {'image':images_uninfected, 'state':labels_uninfected}
 df_uninfected=pd.DataFrame(data=dict_uninf)
 
+#merge infected and uninfected dataframes
 malariadata = pd.concat([df_infected, df_uninfected], ignore_index=True)
 
 """
@@ -34,5 +39,5 @@ malariadata = pd.concat([df_infected, df_uninfected], ignore_index=True)
 print(df.head())
 print(df.tail())
 """
-
+#save dataframe 
 malariadata.to_pickle("./malariadata.pkl")

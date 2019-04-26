583cf917a5ce22a2829791b65ae06ecd30900d11
Jan Stawowy <stawa29@interia.pl>
2019-04-26 12:33:02 +0200

    added comments

--- createdataframeforprediction.py
+++ createdataframeforprediction.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import cv2
 import glob
 
-
+#Set width and height of images
 W=32
 H=32
+#read and resize all images from a folder
 images = [cv2.resize(cv2.imread(file),(W,H)) for file in glob.glob("photosforprediction/*.png")]
 
 """
@@ -18,6 +19,7 @@ print(images[0])
 plt.imshow(images[0])
 plt.show()"""
 
+#create and save dataframe
 dict= {'image':images}
 df=pd.DataFrame(data=dict)
 

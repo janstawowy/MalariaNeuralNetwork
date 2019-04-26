53cf9d08774f735958db60f04a19fcaf5fe2d802
Jan Stawowy <stawa29@interia.pl>
2019-04-26 12:35:07 +0200

    added comments

--- predict.py
+++ predict.py
@@ -8,13 +8,14 @@ from sklearn.model_selection import train_test_split
 from keras.layers.normalization import BatchNormalization
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
+#read dataframe
 data_path='malariapredictdata.pkl'
 df=pd.read_pickle(data_path)
 
-#extract images and labels
+#extract images
 images=np.asarray(list(df['image']))
 
-
+#setup neural network
 model=Sequential()
 model.add(Conv2D(32, kernel_size=(3,3),padding='same', activation='relu', input_shape=(32, 32, 3,)))
 model.add(MaxPool2D(2))
@@ -27,14 +28,16 @@ model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
+#compile and load pretrained weights
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 model.load_weights('malariamodel.h5')
 
-
+#predict if cells are infected or not
 y_pred=model.predict(images)
 y_pred=(y_pred>0.5)
 print(y_pred)
 
+#plot the results
 i=0
 for image in images:
     plt.figure(i+1)
